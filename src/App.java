edba94565dce69ea79430410fa9493c88d6157ca
Cadu Annes <caduannes@gmail.com>
2025-10-03 15:19:17 -0400

    Quarto commit/App

--- src/App.java
+++ src/App.java
@@ -1,5 +1,16 @@
-public class App {
+import java.util.ArrayList;
+
+import Dominio.Cliente;
+import FakeDB.ClienteFakeDB;
+
+public class App{
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        ClienteFakeDB db = new ClienteFakeDB();
+        ArrayList<Cliente> clientes = db.getInstancia();
+        for (Cliente cliente : clientes) {
+            System.out.println("Codigo: "+ cliente.getCodigo());
+            System.out.println("CPF: "+ cliente.getCpf());
+            System.out.println("Email: "+ cliente.getEmail());
+        }
     }
 }
