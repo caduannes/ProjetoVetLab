33f9bc4c91575ec59d96a2c634031cc4c164f0e8
Cadu Annes <caduannes@gmail.com>
2025-10-03 15:11:10 -0400

    Terceiro commit/FakeDB

---
+++ src/FakeDB/ClienteFakeDB.java
@@ -0,0 +1,24 @@
+package FakeDB;
+
+import java.util.ArrayList;
+
+import Dominio.Cliente;
+
+public class ClienteFakeDB extends AbsFakeDB<Cliente>{
+
+    @Override
+    protected void AutoPreencher() {
+        if(this.instancia ==null){
+            this.instancia = new ArrayList<>();
+        }
+        this.instancia.add(new Cliente(1, "Carlos", "123456789", "Caduannes@gmail.com", "123"));
+        this.instancia.add(new Cliente(2, "Gabriela", "987654321", "Gabriela@gmail.com", "321"));
+        this.instancia.add(new Cliente(3, "Matheus", "456852159", "Matheus@gmail.com", "456"));
+        this.instancia.add(new Cliente(4, "Bruno", "789639123", "Bruno@gmail.com", "789"));
+        this.instancia.add(new Cliente(5, "Luiz", "159987456", "Luiz@gmail.com", "963"));
+    }
+    
+    public ClienteFakeDB(){
+        super();
+    }
+}
