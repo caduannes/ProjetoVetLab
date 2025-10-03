c1b962d352263f2593ca2ed7ef1b4a4f6d07be43
Cadu Annes <caduannes@gmail.com>
2025-10-03 14:49:09 -0400

    Segundo commit/dominio

--- src/Dominio/AbsPessoaFisica.java
+++ src/Dominio/AbsPessoaFisica.java
@@ -1,5 +1,30 @@
 package Dominio;
 
-public class AbsPessoaFisica {
+public abstract class AbsPessoaFisica extends AbsIdentificador{
     
+    protected String nome;
+    protected String cpf;
+    protected String email;
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public AbsPessoaFisica(){
+        super();
+    }
+
 }
